87cc5978c84715d719735bd9744ed255bf48e458
lvbibir <15810243114@163.com>
2022-10-07 20:46:55 +0800

    update post

---
+++ upload.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+set -e
+git add .
+git commit -m "update post"
+git push origin master
+
+hugo -F --cleanDestinationDir
+rsync -avuz --progress   --delete public/ root@101.201.150.47:/root/blog/data/hugo/
