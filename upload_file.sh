4cdf6ad9812aa4be27f57a2a1c17cc5ffc2cee1d
lvbibir <15810243114@163.com>
2023-11-14 09:28:51 +0800

    modify bash script

--- upload_file.sh
+++ upload_file.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 set -e
-git add .
-git commit -m "update post"
-git push origin master
 
 hugo -F --cleanDestinationDir
 rsync -avuz --progress   --delete public/ root@lvbibir.cn:/root/blog/data/hugo/
