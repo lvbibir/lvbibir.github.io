2e7db4ced084161d2b5cb90ba61872e17b9c3b8e
lvbibir <15810243114@163.com>
2026-01-06 22:46:44 +0800

    refactor(images): 移除未引用的旧图片, 修复 rsync --delete 失效问题
    - 删除 475 个历史遗留图片文件
    - 修复 update-file.sh 中 rsync 使用 source/* 导致 --delete 不生效的问题

--- update-file.sh
+++ update-file.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rsync -az --info=progress2 --delete --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r /mnt/c/Users/lvbibir/OneDrive/1-lvbibir/obsidian/lvbibir/blog/* content/posts/
-rsync -az --info=progress2 --delete --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r /mnt/c/Users/lvbibir/OneDrive/1-lvbibir/obsidian/lvbibir/images/* static/images/
+rsync -az --info=progress2 --delete --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r /mnt/c/Users/lvbibir/OneDrive/1-lvbibir/obsidian/lvbibir/blog/ content/posts/
+rsync -az --info=progress2 --delete --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r /mnt/c/Users/lvbibir/OneDrive/1-lvbibir/obsidian/lvbibir/images/ static/images/
 
 hugo server -D
